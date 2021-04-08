35a7d61ec71d74dc2ad340dd0b68070e802b0788
Rodrigo <rodrigo.gontijo@hotmail.com>
2021-04-08 02:13:31 -0300

    Add unit tests

--- app/src/main/java/com/example/rateapp/util/util.kt
+++ app/src/main/java/com/example/rateapp/util/util.kt
@@ -1,12 +1,22 @@
 package com.example.rateapp.util
 
+import android.content.ContentValues.TAG
 import android.content.Context
+import android.graphics.drawable.Drawable
+import android.util.Log
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import androidx.vectordrawable.graphics.drawable.Animatable2Compat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.Priority
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.load.resource.gif.GifDrawable
+import com.bumptech.glide.request.RequestListener
 import com.bumptech.glide.request.RequestOptions
+import com.bumptech.glide.request.target.Target
 import com.bumptech.glide.request.target.Target.SIZE_ORIGINAL
 import com.example.rateapp.R
 
@@ -31,10 +41,13 @@ fun ImageView.loadImage(
 
     if (centerCropEnabled)
         Glide.with(context)
-            .setDefaultRequestOptions(options)
+            .applyDefaultRequestOptions(options)
             .load(uri)
             .placeholder(progressDrawable)
             .centerCrop()
+            .dontAnimate()
+            .override(Target.SIZE_ORIGINAL)
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
             .into(this)
     else
         Glide.with(context)
@@ -42,6 +55,17 @@ fun ImageView.loadImage(
             .load(uri)
             .placeholder(progressDrawable)
             .fitCenter()
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(p0: GlideException?, p1: Any?, p2: Target<Drawable>?, p3: Boolean): Boolean {
+                    Log.e("IMAGE_EXCEPTION", "Exception " + p0.toString());
+                    return false
+                }
+                override fun onResourceReady(p0: Drawable?, p1: Any?, p2: Target<Drawable>?, p3: DataSource?, p4: Boolean): Boolean {
+                    Log.d(TAG, "OnResourceReady")
+                    //do something when picture already loaded
+                    return false
+                }
+            })
             .into(this)
 }
 
--- app/src/test/java/com/example/rateapp/ExampleUnitTest.kt
+++ app/src/test/java/com/example/rateapp/ExampleUnitTest.kt
@@ -1,17 +1,79 @@
 package com.example.rateapp
 
+import RxSchedulerRule
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
+import com.example.rateapp.model.raterepository.RateDetailsModel
+import com.example.rateapp.model.raterepository.service.RateApiService
+import com.example.rateapp.viewmodel.RateDetailsViewModel
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import io.mockk.impl.annotations.InjectMockKs
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
+import io.mockk.verify
+
+import io.reactivex.Single
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Before
+import org.junit.Rule
+import org.junit.jupiter.api.extension.ExtendWith
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
+@ExtendWith(MockKExtension::class)
 class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+
+    @Rule
+    @Suppress("unused")
+    @JvmField
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @Rule
+    @JvmField
+    val rxSchedulerRule = RxSchedulerRule()
+
+    @MockK
+    lateinit var rateApiService: RateApiService
+
+    @MockK
+    lateinit var rateMock: Observer<RateDetailsModel>
+
+    @MockK
+    lateinit var loadingMock: Observer<Boolean>
+
+    @MockK
+    lateinit var rateLoadErrorMock: Observer<Boolean>
+
+    @InjectMockKs
+    lateinit var rateDetailsViewModel: RateDetailsViewModel
+
+    @Before
+    fun setup(){
+        MockKAnnotations.init(this)
     }
+
+    @Test
+    fun shouldReturnSucessWhenDetailsRequest(){
+        val response = RateDetailsModel("","", 0, "",  "", 0F, null, "")
+
+        every { rateApiService.getRateDetails(1) } answers { Single.just(response)}
+        every { rateMock.onChanged(any()) } answers { nothing }
+        every { loadingMock.onChanged(any()) } answers { nothing }
+
+        rateDetailsViewModel.rate.observeForever(rateMock)
+        rateDetailsViewModel.loading.observeForever(loadingMock)
+        rateDetailsViewModel.fetchFromRemote(1)
+
+        verify { loadingMock.onChanged(true) }
+        verify { rateMock.onChanged(response) }
+        verify { loadingMock.onChanged(false) }
+    }
+
 }
